bd973cd6fb0ae6b2e1da3994f6c634f6b1883e8b
wakabayshi <satoshitomato0419@gmail.com>
2022-09-20 23:54:52 +0900

    追加2
    step14まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "top#index"
   get"about" => "top#about", as: "about"
+
+  1.upto(18) do |n|
+    get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
+  end
+  
+  resources :members
 end
